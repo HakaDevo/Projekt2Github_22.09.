7393e1bcc0661a925e2a7020e6e940d4b00b1eb7
me <webkenner>
2023-09-22 09:33:04 +0200

    Initial Commit

---
+++ Projekt2Github_22.09./ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Projekt2Github_22.09.
+//
+//  Created by Student on 22.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projekt2Github_22.09./Projekt2Github_22_09_App.swift
@@ -0,0 +1,17 @@
+//
+//  Projekt2Github_22_09_App.swift
+//  Projekt2Github_22.09.
+//
+//  Created by Student on 22.09.23.
+//
+
+import SwiftUI
+
+@main
+struct Projekt2Github_22_09_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
